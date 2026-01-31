fa5709a25c8d37cadbd3bc3a2eac2077958bed73
RemnicL <Meiraycn@163.com>
2026-01-31 03:55:45 -0500

    Fixed Physics issues

--- Script/cat_run_player/cat_run.gd
+++ Script/cat_run_player/cat_run.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed: float = 400.0
 @export var jump_velocity: float = -700.0
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
 var gravity: float = float(ProjectSettings.get_setting("physics/2d/default_gravity"))
 func _physics_process(delta: float) -> void:
 	# 重力
@@ -22,10 +23,17 @@ func _physics_process(delta: float) -> void:
 	
 	if dir != 0:
 		anim.play("Move")
+		print("local:", scale, " global:", global_scale) 
+		print("parent scale:", get_parent().scale if get_parent() else "no parent") # 动画
 		if dir < 0:
 			anim.flip_h = false
+			anim.position.x = 0
+			
 		else:
 			anim.flip_h = true
+			anim.position.x = -73
+			
+			
 	else:
 		anim.stop()
 	#elif dir < 0:
