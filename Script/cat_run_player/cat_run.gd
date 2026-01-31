00e990899fff26a13f74e38dc586c039c19803e7
Maggieyu <maggie.majiaoyu@gmail.com>
2026-01-31 01:21:19 -0500

    Add animations to main characters

---
+++ Script/cat_run_player/cat_run.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+@export var speed: float = 220.0
+@export var jump_velocity: float = -420.0
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+var gravity: float = float(ProjectSettings.get_setting("physics/2d/default_gravity"))
+
+func _physics_process(delta: float) -> void:
+	# 重力
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# 左右输入：-1 / 0 / 1
+	var dir := Input.get_axis("move_left", "move_right")
+	velocity.x = dir * speed
+
+	# 跳跃
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# 移动（Godot 4 的角色标准写法）
+	move_and_slide()
+
+	# 动画
+	if dir > 0:
+		anim.play("Right0")
+	elif dir < 0:
+		anim.play("Left0")
+	else:
+		# 不动时：停在当前帧或你可以做一个 Idle 动画
+		anim.stop()
